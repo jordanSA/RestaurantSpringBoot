c7c6208ed534b4751678eec0dd1f4e575b941e93
Jordan <jordan.abraham127@gmail.com>
2019-02-07 19:34:21 -0500

    R in controller

--- src/main/java/com/tts/Restaurant/controllers/RestaurantController.java
+++ src/main/java/com/tts/Restaurant/controllers/RestaurantController.java
@@ -23,7 +23,7 @@ public class RestaurantController {
 	public String index(Restaurant restaurant, Model model) {
 		restaurants = (List<Restaurant>) restaurantRepository.findAll();
 		model.addAttribute("restaurants", restaurants);
-		return "restaurant/index";
+		return "Restaurant/index";
 	}
 	
 	@GetMapping(value="/new")
